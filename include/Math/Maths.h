633f93776e8c099f803676a2ff0eac34153a389f
Tomáš Kubovčík <t.kubovcik@gmail.com>
2015-11-11 21:22:22 +0200

    Maths include header file

---
+++ include/Math/Maths.h
@@ -0,0 +1,17 @@
+/*
+*   Project:	Forest, rendering using OpenGL
+*	Location:	Lappeenranta University of Technology
+*	Author:		Tomáš Kubov?ík, t.kubovcik@gmail.com
+*	File desc:	Mathematics include header, Matrix4.h contains
+*				includes for other maths structures as vectors
+*				and quaternions
+*/
+
+
+#ifndef FOREST_MATH_MATHS_H
+#define FOREST_MATH_MATHS_H
+
+#include "Matrix4.h"
+#include "Common.h"
+
+#endif	//FOREST_MATH_MATHS_H
