7d34da863baa1f94bc3e60c44e1bb5478a8fc421
Tomáš Kubovčík <t.kubovcik@gmail.com>
2015-11-11 21:22:35 +0200

    Shader program class

---
+++ include/Graphics/Shaders/Shader.h
@@ -0,0 +1,60 @@
+﻿/*
+*   Project:	Forest, rendering using OpenGL
+*	Location:	Lappeenranta University of Technology
+*	Author:		Tomáš Kubovčík, t.kubovcik@gmail.com
+*	File desc:	Shader program class declaration
+*/
+
+
+#ifndef FOREST_GRAPHICS_SHADER_H
+#define FOREST_GRAPHICS_SHADER_H
+
+#include <GL/glew.h>
+#include "Strings.h"
+
+#ifndef FOREST_MATH_MATHS_H
+	#include "Maths.h"
+#endif
+
+using namespace Forest::Math;
+
+namespace Forest
+{
+	namespace Graphics
+	{
+		class Shader
+		{
+		public:
+			static GLuint	LoadShader(const ustring& file, GLuint type);
+			static void		ValidateCompile(GLuint sID);
+
+		public:
+			Shader(const ustring& vs, const ustring& fs);
+			~Shader();
+
+			void	Bind()	const;
+			void	Unbind() const;
+
+		protected:
+			virtual void	GetAllUniformLocations() = 0;
+			GLuint			GetUniformLocation(const GLchar* name)							{ return glGetUniformLocation(m_ProgramID, name); }
+
+			void	BindUniform(const GLuint location, int32_t i) const						{ glUniform1i(location, i); }
+			void	BindUniform(const GLuint location, float f)	const						{ glUniform1f(location, f); }
+			void	BindUniform(const GLuint location, float f1, float f2) const			{ glUniform2f(location, f1, f2); }
+			void	BindUniform(const GLuint location, float f1, float f2, float f3) const	{ glUniform3f(location, f1, f2, f3); }
+			void	BindUniform(const GLuint location, const Vector3& v) const				{ glUniform3f(location, v.x, v.y, v.z); }
+			void	BindUniform(const GLuint location, const Vector4& v) const				{ glUniform4f(location, v.x, v.y, v.z, v.w); }
+			void	BindUniform(const GLuint location, const Matrix4& m) const				{ glUniformMatrix4fv(location, 1, GL_FALSE, &m.c1.x); }
+			void	BindUniform(const GLuint location, bool b) const						{ glUniform1i(location, b ? 1 : 0); }
+
+		private:
+			GLuint		m_ProgramID;
+			GLuint		m_VertexShaderID;
+			GLuint		m_FragmentShaderID;
+		};
+	}
+}
+
+
+#endif	//FOREST_GRAPHICS_SHADER_H
---
+++ src/Graphics/Shaders/Shader.cpp
@@ -0,0 +1,123 @@
+﻿/*
+*   Project:	Forest, rendering using OpenGL
+*	Location:	Lappeenranta University of Technology
+*	Author:		Tomáš Kubovčík, t.kubovcik@gmail.com
+*	File desc:	Shader program class definition
+*/
+
+
+#include "Shader.h"
+
+#include <string>
+#include <fstream>
+#include "StringStream.h"
+
+namespace Forest
+{
+	namespace Graphics
+	{
+		Shader::Shader(const ustring & vs, const ustring & fs)
+		{
+			m_VertexShaderID = LoadShader(vs, GL_VERTEX_SHADER);
+			m_FragmentShaderID = LoadShader(fs, GL_FRAGMENT_SHADER);
+
+			//Create new shader program
+			m_ProgramID = glCreateProgram();
+
+			//Attach vertex and fragment shaders to the shader program
+			glAttachShader(m_ProgramID, m_VertexShaderID);
+			glAttachShader(m_ProgramID, m_FragmentShaderID);
+
+			//Link and validate program
+			glLinkProgram(m_ProgramID);
+			glValidateProgram(m_ProgramID);
+		}
+
+		Shader::~Shader()
+		{
+			Unbind();
+
+			//Detach vertex & fragment shader
+			glDetachShader(m_ProgramID, m_VertexShaderID);
+			glDetachShader(m_ProgramID, m_FragmentShaderID);
+
+			//Delete vertex & fragment shader
+			glDeleteShader(m_VertexShaderID);
+			glDeleteShader(m_FragmentShaderID);
+
+			//Delete program iteself
+			glDeleteProgram(m_ProgramID);
+		}
+
+		void Shader::Bind() const
+		{
+			glUseProgram(m_ProgramID);
+		}
+
+		void Shader::Unbind() const
+		{
+			glUseProgram(0);
+		}
+
+
+		/// <summary>
+		/// Loads the shader from source file.
+		/// </summary>
+		/// <param name="file">Path to the source file.</param>
+		/// <param name="type">Type of shader (VERTEX/FRAGMENT SHADER).</param>
+		/// <returns>Shader ID</returns>
+		GLuint Shader::LoadShader(const ustring & file, GLuint type)
+		{
+			std::ifstream input;
+			input.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+			std::string srcCode;
+
+			try
+			{
+				input.open(file.c_str());
+				srcCode.assign((std::istreambuf_iterator<char>(input)), (std::istreambuf_iterator<char>()));
+			}
+			catch (std::ifstream::failure e)
+			{
+				ucout << "Could not read shader file!\n";
+			}
+
+			GLuint sID = glCreateShader(type);
+			const char * code = srcCode.c_str();
+
+			glShaderSource(sID, 1, (const GLchar**)&code, NULL);
+			glCompileShader(sID);
+
+			ValidateCompile(sID);
+
+			return sID;
+		}
+
+		/// <summary>
+		/// Validates the compilation of shader program and prints errors.
+		/// </summary>
+		/// <param name="sID">ID of shader.</param>
+		void Shader::ValidateCompile(GLuint sID)
+		{
+			GLint compiled;
+
+			glGetShaderiv(sID, GL_COMPILE_STATUS, &compiled);
+
+			if (!compiled)
+			{
+				GLsizei length;
+				glGetShaderiv(sID, GL_INFO_LOG_LENGTH, &length);
+
+				GLchar* log = new GLchar[length + 1];
+				glGetShaderInfoLog(sID, length, &length, log);
+
+				ucout << log << std::endl;
+
+				delete[] log;
+
+				glDeleteShader(sID);
+			}
+		}
+	}
+}
