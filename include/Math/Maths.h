ab517df31b035efdb4b264bfbf329de62349bf40
Tomáš Kubovčík <t.kubovcik@gmail.com>
2015-11-14 14:54:00 +0200

    Added Vector2.h to the header

--- include/Math/Maths.h
+++ include/Math/Maths.h
@@ -1,16 +1,16 @@
-/*
+﻿/*
 *   Project:	Forest, rendering using OpenGL
 *	Location:	Lappeenranta University of Technology
-*	Author:		Tom� Kubov?�k, t.kubovcik@gmail.com
+*	Author:		Tomáš Kubovčík, t.kubovcik@gmail.com
 *	File desc:	Mathematics include header, Matrix4.h contains
 *				includes for other maths structures as vectors
 *				and quaternions
 */
 
-
 #ifndef FOREST_MATH_MATHS_H
 #define FOREST_MATH_MATHS_H
 
+#include "Vector2.h"
 #include "Matrix4.h"
 #include "Common.h"
 
