3a975e95563ec048982b5bc9de52961b0cbf101b
Tomáš Kubovčík <t.kubovcik@gmail.com>
2015-11-14 14:09:03 +0200

    Added static shader class for rendering entities

---
+++ include/Graphics/Shaders/StaticShader.h
@@ -0,0 +1,47 @@
+﻿/*
+*   Project:	Forest, rendering using OpenGL
+*	Location:	Lappeenranta University of Technology
+*	Author:		Tomáš Kubovčík, t.kubovcik@gmail.com
+*	File desc:	Static shader class declaration,
+*				this shader serves as main shader for
+*				rendering world entities
+*/
+
+
+#ifndef FOREST_GRAPHICS_STATICSHADER_H
+#define FOREST_GRAPHICS_STATICSHADER_H
+
+#include "Shader.h"
+
+namespace Forest
+{
+	namespace Graphics
+	{
+		class StaticShader : public Shader
+		{
+		public:
+			StaticShader();
+			~StaticShader();
+
+			void BindModelMatrix(const Matrix4& m) const		{ BindUniform(m_loc_ModelMatrix, m); }
+			void BindViewMatrix(const Matrix4& m) const			{ BindUniform(m_loc_ViewMatrix, m); }
+			void BindProjectionMatrix(const Matrix4& m) const	{ BindUniform(m_loc_ProjectionMatrix, m); }
+			void BindCameraPosition(const Vector3& p) const		{ BindUniform(m_loc_CameraPosition, p); }
+			void BindTextures() const							{ BindUniform(m_loc_DiffuseTexture, 0); BindUniform(m_loc_AlphaTexture, 1); }
+
+		protected:
+			virtual void GetAllUniformLocations() override;
+
+		private:
+			GLuint	m_loc_ModelMatrix;
+			GLuint	m_loc_ViewMatrix;
+			GLuint	m_loc_ProjectionMatrix;
+			GLuint	m_loc_CameraPosition;
+
+			GLuint	m_loc_DiffuseTexture;
+			GLuint	m_loc_AlphaTexture;
+		};
+	}
+}
+
+#endif	//FOREST_GRAPHICS_STATICSHADER_H
---
+++ src/Graphics/Shaders/StaticShader.cpp
@@ -0,0 +1,40 @@
+﻿/*
+*   Project:	Forest, rendering using OpenGL
+*	Location:	Lappeenranta University of Technology
+*	Author:		Tomáš Kubovčík, t.kubovcik@gmail.com
+*	File desc:	Static shader definition
+*/
+
+#include "StaticShader.h"
+
+namespace Forest
+{
+	namespace Graphics
+	{
+		static const ustring VERTEX_SHADER_FILE		= U("simple_vs.glsl");
+		static const ustring FRAGMENT_SHADER_FILE	= U("simple_fs.glsl");
+
+		StaticShader::StaticShader() :
+			Shader(VERTEX_SHADER_FILE, FRAGMENT_SHADER_FILE)
+		{
+			GetAllUniformLocations();
+		}
+
+
+		StaticShader::~StaticShader()
+		{
+		}
+
+
+		void StaticShader::GetAllUniformLocations()
+		{
+			m_loc_ModelMatrix = GetUniformLocation("modelMatrix");
+			m_loc_ViewMatrix = GetUniformLocation("viewMatrix");
+			m_loc_ProjectionMatrix = GetUniformLocation("projectionMatrix");
+			m_loc_CameraPosition = GetUniformLocation("cameraPosition");
+
+			m_loc_DiffuseTexture = GetUniformLocation("diffuseTex");
+			m_loc_AlphaTexture = GetUniformLocation("alphaTex");
+		}
+	}
+}
