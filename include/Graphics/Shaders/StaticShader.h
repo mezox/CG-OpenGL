1751419a723bd5d32ec240b174e31209655ac2e0
Tomáš Kubovčík <t.kubovcik@gmail.com>
2015-11-16 21:54:10 +0200

    Added support for textures, alpha textures, and multiple lights

--- include/Graphics/Shaders/StaticShader.h
+++ include/Graphics/Shaders/StaticShader.h
@@ -12,6 +12,7 @@
 #define FOREST_GRAPHICS_STATICSHADER_H
 
 #include "Shader.h"
+#include "Light.h"
 
 namespace Forest
 {
@@ -20,6 +21,9 @@ namespace Forest
 		class StaticShader : public Shader
 		{
 		public:
+			static const int LIGHT_COUNT = 25;
+
+		public:
 			StaticShader();
 			~StaticShader();
 
@@ -27,7 +31,15 @@ namespace Forest
 			void BindViewMatrix(const Matrix4& m) const			{ BindUniform(m_loc_ViewMatrix, m); }
 			void BindProjectionMatrix(const Matrix4& m) const	{ BindUniform(m_loc_ProjectionMatrix, m); }
 			void BindCameraPosition(const Vector3& p) const		{ BindUniform(m_loc_CameraPosition, p); }
-			void BindTextures() const							{ BindUniform(m_loc_DiffuseTexture, 0); BindUniform(m_loc_AlphaTexture, 1); }
+			void BindHasTextures(bool has) const				{ BindUniform(m_loc_HasTextures, has); }
+			void BindHasTransparency(bool has) const			{ BindUniform(m_loc_HasTransparency, has); }
+			void BindUseFakeLighting(bool val) const			{ BindUniform(m_loc_UseFakeLighting, val); }
+			void BindDiffuse() const							{ BindUniform(m_loc_DiffuseTexture, 0); }
+			void BindAlpha() const								{ BindUniform(m_loc_AlphaTexture, 1); }
+			void BindSpecularVariables(float sd, float r) const { BindUniform(m_loc_ShineDamper, sd); BindUniform(m_loc_Reflectivity, r);}
+			void BindSkyColor(const Vector3& color) const		{ BindUniform(m_loc_SkyColor, color); }
+			void BindLights(const vector<Light>& l) const;
+
 
 		protected:
 			virtual void GetAllUniformLocations() override;
@@ -38,6 +50,18 @@ namespace Forest
 			GLuint	m_loc_ProjectionMatrix;
 			GLuint	m_loc_CameraPosition;
 
+			GLuint	m_loc_LightPosition[LIGHT_COUNT];
+			GLuint	m_loc_LightColor[LIGHT_COUNT];
+			GLuint	m_loc_LightAttenuation[LIGHT_COUNT];
+			GLuint	m_loc_LightAmbientIntensity;
+
+			GLuint	m_loc_ShineDamper;
+			GLuint	m_loc_Reflectivity;
+			GLuint	m_loc_SkyColor;
+
+			GLuint	m_loc_HasTextures;
+			GLuint	m_loc_HasTransparency;
+			GLuint	m_loc_UseFakeLighting;
 			GLuint	m_loc_DiffuseTexture;
 			GLuint	m_loc_AlphaTexture;
 		};
--- src/Graphics/Shaders/StaticShader.cpp
+++ src/Graphics/Shaders/StaticShader.cpp
@@ -25,16 +25,53 @@ namespace Forest
 		{
 		}
 
+		void StaticShader::BindLights(const vector<Light>& l) const
+		{
+			for (int i = 0; i < LIGHT_COUNT; i++)
+			{
+				if (i < l.size())
+				{
+					BindUniform(m_loc_LightPosition[i], l.at(i).Position());
+					BindUniform(m_loc_LightColor[i], l.at(i).Color());
+					BindUniform(m_loc_LightAttenuation[i], l.at(i).Attenuation());
+				}
+				else
+				{
+					BindUniform(m_loc_LightPosition[i], Vector3());
+					BindUniform(m_loc_LightColor[i], Vector3());
+					BindUniform(m_loc_LightAttenuation[i], Vector3(1.0f,0.0f,0.0f));
+				}
+			}
+			
+			BindUniform(m_loc_LightAmbientIntensity, 0.3f);
+		}
+
 
 		void StaticShader::GetAllUniformLocations()
 		{
-			m_loc_ModelMatrix = GetUniformLocation("modelMatrix");
-			m_loc_ViewMatrix = GetUniformLocation("viewMatrix");
-			m_loc_ProjectionMatrix = GetUniformLocation("projectionMatrix");
-			m_loc_CameraPosition = GetUniformLocation("cameraPosition");
+			m_loc_ModelMatrix		= GetUniformLocation("modelMatrix");
+			m_loc_ViewMatrix		= GetUniformLocation("viewMatrix");
+			m_loc_ProjectionMatrix	= GetUniformLocation("projectionMatrix");
+			m_loc_CameraPosition	= GetUniformLocation("cameraPosition");
 
-			m_loc_DiffuseTexture = GetUniformLocation("diffuseTex");
-			m_loc_AlphaTexture = GetUniformLocation("alphaTex");
+			m_loc_HasTransparency	= GetUniformLocation("hasTransparency");
+			m_loc_HasTextures		= GetUniformLocation("hasTextures");
+			m_loc_UseFakeLighting	= GetUniformLocation("useFakeLighting");
+			m_loc_DiffuseTexture	= GetUniformLocation("diffuseTex");
+			m_loc_AlphaTexture		= GetUniformLocation("alphaTex");
+
+			m_loc_ShineDamper		= GetUniformLocation("shineDamper");
+			m_loc_Reflectivity		= GetUniformLocation("reflectivity");
+			
+			for (int i = 0; i < LIGHT_COUNT; i++)
+			{
+				m_loc_LightPosition[i]		= GetUniformLocation(("lightPosition[" + to_string(i) + "]").c_str());
+				m_loc_LightColor[i]			= GetUniformLocation(("lColor[" + to_string(i) + "]").c_str());
+				m_loc_LightAttenuation[i]	= GetUniformLocation(("lAttenuation[" + to_string(i) + "]").c_str());
+			}
+
+			m_loc_LightAmbientIntensity = GetUniformLocation("lAmIntensity");
+			m_loc_SkyColor = GetUniformLocation("skyColor");
 		}
 	}
 }
